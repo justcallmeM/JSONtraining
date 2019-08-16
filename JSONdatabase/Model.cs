695dccc425cf633589a695aaf9720e8e5fa7e9ce
Mindaugas Pikelis <mindaugas.pikelis@atea.lt>
2019-08-16 09:33:45 +0300

    project

---
+++ JSONdatabase/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace trainingJSON
+{
+    public class Product
+    {
+        public string imagePath { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+        public string description { get; set; }
+        public List<string> photos { get; set; }
+    }
+    public class ProductList
+    {
+        public List<Product> productList { get; set; }
+    }
+    public class Database
+    {
+        public ProductList database { get; set; }
+    }
+}
---
+++ JSONdatabase/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace trainingJSON
+{
+    class Program
+    {
+        static void Main()
+        {
+            var readings = File.ReadAllText(@"C:\Users\mindaugas.pikelis\source\repos\trainingJSON\trainingJSON\database.json");
+            var database = JsonConvert.DeserializeObject<Database>(readings); //object pakeist i top level
+        }
+    }
+}
